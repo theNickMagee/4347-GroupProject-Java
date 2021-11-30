f5c11d59439e6754b8dcc4b43e8c85a069b34f5c
rkw180000 <80798212+rkw180000@users.noreply.github.com>
2021-11-30 12:24:41 -0600

    Update Project4347Phase4.java

--- Project4347Phase4.java
+++ Project4347Phase4.java
@@ -39,6 +39,7 @@ public class Project4347Phase4 {
             System.out.println("Select from options below: ");
             System.out.println("1. Insert Player on Team.");
             System.out.println("2. Insert Staff Member on Team.");
+            System.out.println("3. Insert Game Event.");
             // INSERT MORE OPTIONS HERE
             System.out.println("6. Exit.");
             System.out.println("Enter a number: ");
@@ -47,6 +48,29 @@ public class Project4347Phase4 {
             // GO TO SQL FUNTION HERE
             if (n == 1) {
 
+            }
+            
+            if ( n == 3 ) {
+                // Open a connection
+	             try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+	             Statement stmt = conn.createStatement();
+	             ) {		      
+	    	 @SuppressWarnings("resource")
+			 Scanner input = new Scanner(System.in);
+	         // Execute a query
+	         System.out.print("Please enter the date of the Event as YYYY-MM-DD: ");          
+	         String sql = "INSERT INTO GAME_EVENT VALUES (";
+	         sql +=	input.nextLine() + ", ";
+	         System.out.print("Please enter the winning teams name: ");          
+	         sql +=	input.nextLine() + ", ";
+	         System.out.print("Please enter the losing teams name: ");          
+	         sql +=	input.nextLine() + ")";
+	         stmt.executeUpdate(sql);
+	         System.out.println("New game event was added to the database.");
+	         
+	      } catch (SQLException e) {
+	         e.printStackTrace();
+	      }
             }
 
             if (n == 6) {
